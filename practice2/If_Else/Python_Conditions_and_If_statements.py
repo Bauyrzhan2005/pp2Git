7196e0e7f101272b708d585cf495ca6f37ca4098
Zhangbyrbay Bauyrzhan <b.dzhanbirbay2005@gmail.com>
2026-02-17 17:26:05 +0500

    Practice 2

---
+++ practice2/If_Else/Python_Conditions_and_If_statements.py
@@ -0,0 +1,30 @@
+#1
+a = 33
+b = 200
+if b > a:
+   print("b is greater than a")
+
+#2 How If Statements Work
+number = 15
+if number > 0:
+   print("The number is positive")
+
+#3 Indentation
+a = 33
+b = 200
+if b > a:
+   print("b is greater than a") # you will get an error
+
+#4 Multiple Statements in If Block
+age = 20
+if age >= 18:
+   print("You are an adult")
+   print("You can vote")
+   print("You have full legal rights")
+
+#5 Using Variables in Conditions
+is_logged_in = True
+if is_logged_in:
+   print("Welcome back!")
+
+   
